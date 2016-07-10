01a45fc35db6c402c9ca22f525c86ad6c31ad22d
linkviii <LinkEighth@gmail.com>
2016-07-10 17:18:50 -0400

    moved files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 let draw = SVG('drawing')//.size(300, 300);
 let rect = draw.rect(100, 100).attr({fill: 'green', stroke: 'blue'});
 
-let sample = "simple_timeline.json";
-let anime = "no.json";
+let sample = "res/simple_timeline.json";
+let anime = "res/no.json";
 let tl = new Timeline(sample, "sampleTimeline");
 let t2 = new Timeline(anime, "animeTimeline");
 //console.log( tl.data);
